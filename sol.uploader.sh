8dc37f6a4b3b123bb83e2d48a72413732870df1e
Radim Kučera <rkucera@gmail.com>
2025-03-28 18:33:36 +0100

    ...

--- sol.uploader.sh
+++ sol.uploader.sh
@@ -6,3 +6,5 @@ set -e
 scp media/video/tate/* zero@10.0.0.211:/home/zero/solrunners/media/video/tate/
 scp -r media/static/ zero@10.0.0.211:/home/zero/solrunners/media/static
 scp -r media/templates/ zero@10.0.0.211:/home/zero/solrunners/media/templates
+
+scp media/audio/tate/* zero@10.0.0.211:/home/zero/solrunners/media/audio/tate/
