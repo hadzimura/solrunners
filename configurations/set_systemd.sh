3f31bff196d797550f8d661d9cd93ee0763ac3c5
Radim Kučera <rkucera@gmail.com>
2025-04-09 22:38:10 +0200

    ...

---
+++ configurations/set_systemd.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# © 2025 Radim Kučera (rkucera@gmail.com)
+
+set -e
+
+sudo cp /home/zero/solrunners/configurations/systemd/sol.service /etc/systemd/system/
+sudo cp /home/zero/solrunners/configurations/systemd/sol-watcher.service /etc/systemd/system/
+sudo cp /home/zero/solrunners/configurations/systemd/sol-watcher.path /etc/systemd/system/
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable sol.service
+sudo systemctl enable sol-watcher.service
+
+sudo systemctl start sol.service
+sudo systemctl start sol-watcher.service
