933984fbc7b3d14ea801624a7795191feffe69d4
Radim Kučera <rkucera@gmail.com>
2025-04-13 21:01:33 +0200

    ...

--- configurations/systemd/media_to_shm.sh
+++ configurations/systemd/media_to_shm.sh
@@ -5,8 +5,8 @@ set -e
 
 SOURCE_VIDEO="/home/zero/solrunners/media/video"
 SOURCE_AUDIO="/home/zero/solrunners/media/audio"
-SOURCE_AUDIO="/home/zero/solrunners/media/static"
-SOURCE_AUDIO="/home/zero/solrunners/media/templates"
+SOURCE_STATIC="/home/zero/solrunners/media/static"
+SOURCE_TEMPLATES="/home/zero/solrunners/media/templates"
 DESTINATION="/storage"
 
 echo "Syncing all the SoL Media from '${SOURCE}' into the SHM storage '${DESTINATION}'"
@@ -19,5 +19,7 @@ fi
 
 cp -r "${SOURCE_VIDEO}" "${DESTINATION}"
 cp -r "${SOURCE_AUDIO}" "${DESTINATION}"
+cp -r "${SOURCE_STATIC}" "${DESTINATION}"
+cp -r "${SOURCE_TEMPLATES}" "${DESTINATION}"
 
 echo "All done"
