cb4291bee00a327f1e1c4b39318786f56b2eb9da
Radim Kučera <rkucera@gmail.com>
2025-04-15 20:41:22 +0200

    ...

--- modules/VideoPlayer.py
+++ modules/VideoPlayer.py
@@ -301,8 +301,8 @@ async def entropy(cfg):
         # This actually controls the playback speed!
         cv.waitKey(frame_time)
 
-        if frame_counter % 125 == 0:
-            video.set(cv.CAP_PROP_POS_FRAMES, randint(1000,7000))
+        # if frame_counter % 125 == 0:
+        #     video.set(cv.CAP_PROP_POS_FRAMES, randint(1000,7000))
 
         if frame_counter == 1:
             print('----')
